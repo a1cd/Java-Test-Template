130577e36fb3942a99118dcb5be627d5128d5cdc
a1cd <71281043+a1cd@users.noreply.github.com>
2021-11-04 17:43:03 -0400

    workflow stuff

---
+++ src/test/java/Jeo/kim.java
@@ -0,0 +1,11 @@
+package Jeo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class kim {
+    @Test
+    public void doTest() {
+        Assertions.assertEquals(1,1);
+    }
+}
